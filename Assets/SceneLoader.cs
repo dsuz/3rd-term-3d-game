f2d1126dc1ddf2135ee50d409979d09f666c0553
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-11-14 03:01:43 +0900

    タイトル画面を作る

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] Color m_fadeColor = Color.black;
+    [SerializeField] float m_fadeMultiplier = 1f;
+
+    /// <summary>
+    /// フェードしながらシーンを切り替える
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadScene(string sceneName)
+    {
+        Initiate.Fade(sceneName, m_fadeColor, m_fadeMultiplier);
+    }
+}
